a3e56076848c69f30e42570f248c4622eb00a483
Lucas Eduardo <lucas.2013costa@hotmail.com>
2023-10-04 15:20:41 -0400

    alterações no front

--- backend/src/FileServer.java
+++ backend/src/FileServer.java
@@ -1,23 +1,51 @@
-import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import static spark.Spark.*;
 
 public class FileServer {
     public static void main(String[] args) {
-        try {
-            // Crie o registro RMI na porta 1099
-            LocateRegistry.createRegistry(1099);
+        // Configurar as rotas do servidor Spark
+        get("/listar-arquivos", listarArquivosRoute);
+        post("/upload-arquivo", uploadArquivoRoute);
+        // Outras rotas podem ser adicionadas para as operações restantes
 
-            // Crie uma instância do serviço e o registre com o nome "FileService"
-            FileService fileService = new FileServiceImpl();
-            Naming.rebind("FileService", fileService);
-
-            // Inicie a API HTTP
-            FileServerAPI fileServerAPI = new FileServerAPI(fileService);
-            fileServerAPI.start();
-
-            System.out.println("Servidor pronto para receber solicitações.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Iniciar o servidor Spark
+        init();
     }
+
+    // Rota para listar os arquivos
+    private static Route listarArquivosRoute = (Request request, Response response) -> {
+        // Lógica para listar os arquivos no servidor
+        List<String> files = // Obtenha a lista de arquivos do sistema de arquivos
+        return files;
+    };
+
+    // Rota para fazer upload de arquivo
+    private static Route uploadArquivoRoute = (Request request, Response response) -> {
+        try {
+            // Obter o arquivo enviado na requisição
+            Path tempDir = Files.createTempDirectory("temp");
+            request.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement(tempDir.toString()));
+            Part filePart = request.raw().getPart("file");
+            String fileName = filePart.getSubmittedFileName();
+            Path filePath = Paths.get("caminho/para/armazenar/os/arquivos", fileName);
+
+            // Salvar o arquivo no sistema de arquivos
+            try (InputStream inputStream = filePart.getInputStream()) {
+                Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            return "Upload de arquivo concluído com sucesso.";
+        } catch (Exception e) {
+            response.status(500);
+            return "Erro ao fazer upload de arquivo: " + e.getMessage();
+        }
+    };
 }
--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const listarArquivosButton = document.querySelector('#listar-arquivos-button');
 const listaArquivosDiv = document.querySelector('#lista-arquivos');
 
 listarArquivosButton.addEventListener('click', () => {
-    fetch('http://endereco-do-servidor:porta/listar-arquivos', {
+    fetch('http://localhost:8080/listar-arquivos', {
         method: 'GET',
     })
     .then(response => response.json())
@@ -14,3 +14,25 @@ listarArquivosButton.addEventListener('click', () => {
         console.error('Erro ao listar arquivos:', error);
     });
 });
+
+const uploadArquivoButton = document.querySelector('#upload-arquivo-button');
+const arquivoInput = document.querySelector('#arquivo-input');
+
+uploadArquivoButton.addEventListener('click', () => {
+    const file = arquivoInput.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('http://localhost:8080/upload-arquivo', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Upload de arquivo concluído:', data);
+    })
+    .catch(error => {
+        console.error('Erro ao fazer upload de arquivo:', error);
+    });
+});
+
